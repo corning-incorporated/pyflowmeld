93866a7e66514dcf71d6231be3d06f8dbf77094b
haddadigh <haddadigh@corning.com>
2025-10-23 16:08:03 -0400

    test for utils/tools load_reshape_trim is added; this is a central function in postprocessing

---
+++ tests/test_utils/test_tools.py
@@ -0,0 +1,41 @@
+# ################################################################################## #
+#  Copyright 2025. Corning Incorporated. All rights reserved.                        #
+#                                                                                    #
+#  This software may only be used in accordance with the identified license(s).      #
+#                                                                                    #   
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR        #
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,          #
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL           #
+#  CORNING BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN        #
+#  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN                 #
+#  CONNECTION WITH THE SOFTWARE OR THE USE OF THE SOFTWARE.                          #
+#  ################################################################################# #
+# ##################################################### #
+# Authors:                                              #
+# Hamed Haddadi Staff Scientist                         #
+#               haddadigh@corning.com                   #
+# David Heine   Principal Scientist and Manager         #
+#               heinedr@corning.com                     #
+# ##################################################### #
+from os import path 
+import numpy as np
+from pyflowmeld.utils.tools import load_reshape_trim
+
+#-- test basic functions of load_reshape_trim --#
+def test_load_reshape_trim_works_with_no_trim(temp_dir):
+    """
+    Tests if the main file has no padding
+    The function should preserve the original shape
+    Test:
+        1) first creates a flat array
+        2) function reads tha array file and reshapes and trims it
+    """
+    shape = (8,12,32)
+    arr = np.arange(np.prod(shape)).reshape(shape)
+    file_path = path.join(temp_dir, "array.txt")
+    np.savetxt(file_path, arr.flatten())
+
+    result = load_reshape_trim(file_path, shape, trim = [0]*6)
+    assert np.array_equal(result, arr), "recovered array is not equal to the original shape"
+    assert result.shape == shape, "recovered shape is not equal to the shape of original array"
+
